c394114c60fe2096d29c030977c3a03e42ae222e
Sumon Chattopadhyay <16009860+schatto1@users.noreply.github.com>
2018-03-02 16:38:48 -0500

    Adds test file Adds code that opens and reads the test file Adds code that opens a write file

---
+++ quoteWrapper.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  2 16:27:21 2018
+
+@author: schattop
+"""
+
+readFile = open("20180228 ADMET2 ES113=1 list.txt")
+
+readContent = readFile.read()
+
+print(readContent)
