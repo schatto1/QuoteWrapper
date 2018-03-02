1675cf4d331c985e4487119f17ba55891f3631f0
Sumon Chattopadhyay <16009860+schatto1@users.noreply.github.com>
2018-03-02 16:53:12 -0500

    Adds comments to certain nice-to-have features

--- quoteWrapper.py
+++ quoteWrapper.py
@@ -5,6 +5,9 @@ Created on Fri Mar  2 16:27:21 2018
 @author: schattop
 """
 
+# ToDo:  use arguments so that user can specify file path when
+# running script. Can also have defaults in case user does not
+# specify any path
 readFile = open("20180228 ADMET2 ES113=1 list.txt", "r")
 writeFile = open("modifiedForSQL.txt", "w+")
 
